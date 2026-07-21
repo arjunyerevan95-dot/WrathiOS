c154f4f023c80838f5278f1ffb96d3b7f8458b10
arjunyerevan95-dot <arjunyerevan95@gmail.com>
2026-07-21 23:36:07 +0530

    Keep UIKit main while marking SDL ready

--- App/main.mm
+++ App/main.mm
@@ -4,6 +4,7 @@
 #import "AppDelegate.h"
 
 #if WRATH_GATE3_DIAGNOSTIC
+#define SDL_MAIN_HANDLED 1
 #include <SDL_main.h>
 #endif
 
@@ -12,7 +13,7 @@ int main(int argc, char *argv[]) {
 #if WRATH_GATE3_DIAGNOSTIC
         // UIKit remains the application owner. SDL2main is disabled in the
         // static library, so explicitly satisfy SDL's custom-entry-point contract
-        // before any subsystem is initialized from AppDelegate.
+        // without allowing SDL_main.h to rename this function.
         SDL_SetMainReady();
 #endif
         return UIApplicationMain(argc, argv, nil, NSStringFromClass(AppDelegate.class));

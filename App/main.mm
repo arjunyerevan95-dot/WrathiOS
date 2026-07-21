56c6528ddd9b0d6b411f3ff332e8c7048c8cabdf
arjunyerevan95-dot <arjunyerevan95@gmail.com>
2026-07-21 20:50:39 +0530

    Add UIKit application entry point

---
+++ App/main.mm
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass(AppDelegate.class));
+    }
+}

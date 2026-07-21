4a1a1e246ce89b9f9861b9783f74a64a2b5a042a
arjunyerevan95-dot <arjunyerevan95@gmail.com>
2026-07-21 22:30:26 +0530

    Fetch the pinned FreeType source

--- scripts/fetch_upstream.sh
+++ scripts/fetch_upstream.sh
@@ -49,6 +49,7 @@ fetch_exact_revision wrath-qc "$WRATH_QC_URL" "$WRATH_QC_COMMIT"
 fetch_exact_revision SDL2 "$SDL2_URL" "$SDL2_COMMIT"
 fetch_exact_revision ogg "$OGG_URL" "$OGG_COMMIT"
 fetch_exact_revision vorbis "$VORBIS_URL" "$VORBIS_COMMIT"
+fetch_exact_revision freetype "$FREETYPE_URL" "$FREETYPE_COMMIT"
 
 "$ROOT_DIR/scripts/verify_upstream.sh"
 python3 "$ROOT_DIR/scripts/validate_engine_manifest.py" --require-upstream

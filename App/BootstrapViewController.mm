f8549d877c35c705946d57c6adc1a717c3e38607
arjunyerevan95-dot <arjunyerevan95@gmail.com>
2026-07-21 23:10:18 +0530

    Launch Gate 3 graphics diagnostic from bootstrap shell

--- App/BootstrapViewController.mm
+++ App/BootstrapViewController.mm
@@ -2,6 +2,11 @@
 
 #import "BootstrapViewController.h"
 #import "WrathEngineBridge.h"
+#import "WrathGraphicsDiagnostic.h"
+
+@interface BootstrapViewController ()
+@property(nonatomic, assign) BOOL gate3LaunchRequested;
+@end
 
 @implementation BootstrapViewController
 
@@ -17,14 +22,22 @@
 
     UILabel *status = [[UILabel alloc] init];
     status.translatesAutoresizingMaskIntoConstraints = NO;
+#if WRATH_GATE3_DIAGNOSTIC
+    status.text = @"Preparing the Gate 3 SDL2/OpenGL ES diagnostic…";
+#else
     status.text = WrathEngineStatusText();
+#endif
     status.font = [UIFont preferredFontForTextStyle:UIFontTextStyleBody];
     status.textAlignment = NSTextAlignmentCenter;
     status.numberOfLines = 0;
 
     UILabel *boundary = [[UILabel alloc] init];
     boundary.translatesAutoresizingMaskIntoConstraints = NO;
+#if WRATH_GATE3_DIAGNOSTIC
+    boundary.text = @"No commercial WRATH data is loaded. Engine startup remains disabled.";
+#else
     boundary.text = @"Commercial WRATH data is not bundled. A later milestone will import files from the user's own licensed installation.";
+#endif
     boundary.font = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
     boundary.textAlignment = NSTextAlignmentCenter;
     boundary.numberOfLines = 0;
@@ -45,6 +58,19 @@
         [stack.trailingAnchor constraintLessThanOrEqualToAnchor:guide.trailingAnchor constant:-32.0],
         [stack.widthAnchor constraintLessThanOrEqualToConstant:700.0]
     ]];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+#if WRATH_GATE3_DIAGNOSTIC
+    if (self.gate3LaunchRequested) {
+        return;
+    }
+    self.gate3LaunchRequested = YES;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.15 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        (void)WrathGraphicsDiagnosticStart();
+    });
+#endif
 }
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
